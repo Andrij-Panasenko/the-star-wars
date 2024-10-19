d079dea4345f68079c6d555a874673e5ff45e938
VladProtchenko <andrii.panasenko295@gmail.com>
2024-10-19 22:31:50 +0300

    add starship and film types

--- src/types/types.ts
+++ src/types/types.ts
@@ -16,3 +16,43 @@ export type Character = {
   edited: string;
   url: string;
 }
+
+export type Starship = {
+  id: number,
+  name: string,
+  model: string,
+  manufacturer: string,
+  cost_in_credits: string,
+  length: string,
+  max_atmosphering_speed: string,
+  crew: string,
+  passengers: string,
+  cargo_capacity: string,
+  consumables: string,
+  hyperdrive_rating: string,
+  MGLT: string,
+  starship_class: string,
+  pilots: number[],
+  films: number[],
+  created: string,
+  edited: string,
+  url: string
+}
+
+export type Film = {
+  id: number,
+  title: string,
+  episode_id: number,
+  opening_crawl: string,
+  director: string,
+  producer: string,
+  release_date: string,
+  characters: number[],
+  planets: number[],
+  starships: number[],
+  vehicles: number[],
+  species: number[],
+  created: string,
+  edited: string,
+  url: string
+}
