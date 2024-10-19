6447d811c8c5ace3754956ece49c68ef0722dc75
VladProtchenko <andrii.panasenko295@gmail.com>
2024-10-19 20:28:20 +0300

    Update CharacterCard.tsx

--- src/components/CharacterCard/CharacterCard.tsx
+++ src/components/CharacterCard/CharacterCard.tsx
@@ -1,15 +1,23 @@
 import { Character } from 'types/types';
 import style from './CharacterCard.module.css'
+import { useAppDispatch } from '../../hooks/useAppDispatch';
+import { fetchCharacterById } from '../../redux/operations';
+import { useNavigate } from 'react-router-dom';
 
 export interface CharacterCardProps {
     characterData: Character;
 }
 
 export default function CharacterCard({ characterData }: CharacterCardProps) {
-    const {name} = characterData;
+    const {name, id} = characterData;
+    const dispatch = useAppDispatch()
+    const navigate = useNavigate()
     return (
         <>
-            <li className={style.character_item}>
+            <li
+                className={style.character_item}
+                onClick={() => { dispatch(fetchCharacterById(id)); navigate(`/character/${id}`);}}
+            >
                 <h1>{name}</h1>
             </li>
         </>
