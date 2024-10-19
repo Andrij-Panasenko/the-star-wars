feac4a619c93a8bc627fb3767f0b0b8ce5c1405b
VladProtchenko <andrii.panasenko295@gmail.com>
2024-10-19 20:27:59 +0300

    add new operatiion fetchCharacterById

--- src/redux/operations.ts
+++ src/redux/operations.ts
@@ -3,6 +3,8 @@ import axios, { AxiosError } from "axios";
 
 axios.defaults.baseURL = 'https://sw-api.starnavi.io'
 
+// getting all characters by pages
+// 10 characters by page
 export const fetchAllCharacters = createAsyncThunk(
   'characters/fetch',
   async (page: number = 1, thunkAPI) => {
@@ -10,7 +12,21 @@ export const fetchAllCharacters = createAsyncThunk(
        const response = await axios.get(`/people?page=${page}`)
     return response.data
     } catch (error) {
-       const typedError = error as AxiosError;
+      const typedError = error as AxiosError;
+      return thunkAPI.rejectWithValue(typedError.message)
+    }
+  }
+)
+
+//request to get data for one certain character
+export const fetchCharacterById = createAsyncThunk(
+  'characters/fetchById',
+  async (id: number, thunkAPI) => {
+    try {
+      const response = await axios.get(`/people/${id}`)
+      return response.data
+    } catch (error) {
+      const typedError = error as AxiosError;
       return thunkAPI.rejectWithValue(typedError.message)
     }
   }
