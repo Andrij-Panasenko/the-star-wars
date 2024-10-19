aa8f579dd10164031b68513a9fc034e973d112f8
Andrii Panasenko <130774640+Andrij-Panasenko@users.noreply.github.com>
2024-10-19 10:50:07 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# vite-react-typescript-template
+## This template was created by using npm create vite@latest react typescript
---
+++ vite.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react';
+import svgr from 'vite-plugin-svgr';
+
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), svgr()],
+  resolve: {
+    alias: {
+      src: '/src',
+      components: '/src/components',
+      pages: '/src/pages',
+      assets: '/src/assets'
+    }
+  },
+  base: '/vite-react-typescript-template' //don't forget to change this line after using template
+})
