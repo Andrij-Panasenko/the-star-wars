9afca3fee8b640c07de27cb4d29e6042a2ce31c5
VladProtchenko <andrii.panasenko295@gmail.com>
2024-10-19 19:29:09 +0300

    add characters list
    render list
    add buttton "load more"
    some fixes in file naming

--- src/App.tsx
+++ src/App.tsx
@@ -3,20 +3,22 @@ import { useEffect } from 'react';
 import { fetchAllCharacters } from './redux/operations';
 import { Route, Routes } from 'react-router-dom';
 import SharedLayout from 'components/SharedLayout/SharedLayout';
-import MainPage from 'pages/MainPage';
+import MainPage from 'pages/MainPage/MainPage';
 
 function App() {
     const dispatch = useAppDispatch();
     useEffect(() => {
         dispatch(fetchAllCharacters(1));
     }, [dispatch]);
-    return <>
-        <Routes>
-            <Route path="/" element={<SharedLayout/>}>
-                <Route index element={<MainPage/>} />
-            </Route>
-        </Routes>
-    </>;
+    return (
+        <>
+            <Routes>
+                <Route path="/" element={<SharedLayout />}>
+                    <Route index element={<MainPage />} />
+                </Route>
+            </Routes>
+        </>
+    );
 }
 
 export default App;
--- src/components/CharacterCard/CharacterCard.tsx
+++ src/components/CharacterCard/CharacterCard.tsx
@@ -1,11 +1,17 @@
-export interface CharacterCardProps {
-    
-} 
+import { Character } from 'types/types';
+import style from './CharacterCard.module.css'
 
-export default function CharacterCard()  {
+export interface CharacterCardProps {
+    characterData: Character;
+}
+
+export default function CharacterCard({ characterData }: CharacterCardProps) {
+    const {name} = characterData;
     return (
         <>
-            <li></li>
+            <li className={style.character_item}>
+                <h1>{name}</h1>
+            </li>
         </>
     );
 }
