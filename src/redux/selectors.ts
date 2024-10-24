732eaa9dc5f709f1f63efb12bd1d7834903d6393
VladProtchenko <andrii.panasenko295@gmail.com>
2024-10-24 18:47:25 +0300

    Update selectors.ts

--- src/redux/selectors.ts
+++ src/redux/selectors.ts
@@ -14,5 +14,6 @@ export const selectStarshipDetails = (state: RootState) => state.starWarsState.s
 
 // select booleat value isLoading
 export const selectIsLoading = (state: RootState) => state.starWarsState.isLoading;
+
 //page selector
 export const selectPage = (state: RootState) => state.starWarsState.page;
