1b5d20d6679f186d01206849aeb32ccd345aa44a
Andrij-Panasenko <130774640+Andrij-Panasenko@users.noreply.github.com>
2024-11-12 12:13:06 +0000

    Deploying to gh-pages from @ Andrij-Panasenko/the-star-wars@75b3329c74b9f676d3800a27ce269f72903701e5 🚀

---
+++ assets/useAppDispatch-Dv2gXMMu.js
@@ -0,0 +1,1 @@
+import{i as s}from"./index-BDrFG-lb.js";const e=a=>a.starWarsState.data,r=a=>a.starWarsState.characterDetails,c=a=>a.starWarsState.filmDetails,i=a=>a.starWarsState.starshipDetails,l=a=>a.starWarsState.page,o=()=>s();export{l as a,r as b,c,i as d,e as s,o as u};
